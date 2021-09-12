627639689021b8f8e26fd08c7a71ec384435b8a4
Vasiliy Myasnikov <fraer747@gmail.com>
2021-09-12 13:56:50 +0300

    1 задание по искл

---
+++ domaska/hello_user_v2.py
@@ -0,0 +1,14 @@
+#Задание 1
+""" 
+def user_fun():
+    try:
+        while True:
+            user_say = input('Как дела? ')
+            if user_say == 'Хорошо':
+                break#цикл закончен
+            else:
+                print('Давай ещё {}'.format(user_say))
+    except KeyboardInterrupt:
+        print("Пока!")
+        
+user_fun() """
